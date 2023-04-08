15b8c678d9f1ff140a56b0a6949d5174e8806f30
Stephen Higgins <stephen.higgins1995@gmail.com>
2023-04-08 05:43:11 +0100

    formatting

--- App.tsx
+++ App.tsx
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import type { PropsWithChildren } from 'react';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Body from './src/library/body/body';
@@ -13,10 +5,6 @@ import { BodyType } from './src/library/body/body.type';
 import Button from './src/library/button/button';
 import { ButtonType } from './src/library/button/button.type';
 import { deleteItem, getItem, setItem } from './src/plugins/storage';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
 function App(): JSX.Element {
   const store = () => {
@@ -31,7 +19,7 @@ function App(): JSX.Element {
     console.log('deleted item');
   };
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', paddingHorizontal: 20 }}>
+    <View style={styles.page}>
       <Body type={BodyType.Normal} text="Hello world Normal" />
       <Body type={BodyType.Bold} text="Hello world Bold" />
       <Body type={BodyType.Small} text="Hello world Small" />
@@ -46,22 +34,7 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  page: { flex: 1, justifyContent: 'center', alignItems: 'center', paddingHorizontal: 20 },
 });
 
 export default App;
